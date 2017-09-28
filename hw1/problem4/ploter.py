4dcd6a6a46e5d2443a64d24bec81cfc2d998ff7a
Michael Morse <mjmorse3@buffalo.edu>
2017-09-28 17:27:43 -0400

    opps

--- hw1/problem4/ploter.py
+++ hw1/problem4/ploter.py
@@ -1,16 +1,16 @@
 from matplotlib import pyplot;
 #from pylab import genfromtxt;
 import numpy as np
-import math as mth
 #mat0 = genfromtxt("data0.txt");
+#data = np.genfromtxt("problem4.txt",delimiter=",");
 data = np.genfromtxt("problem4.txt",delimiter=",");
-#print mat1[1:3,:]
+print data[1:,1]
 mytime = pyplot.scatter(np.log(data[1:,0]/np.log(4.)), data[1:,1],marker=".");
 other  = pyplot.scatter(np.log(data[1:,0]/np.log(4.)), data[1:,2],marker=".");
-pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,1],marker=".");
-pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,2],marker=".");
-labels = [1,4,16,256,'1K','4K','16K','64K','256K','1M','4M','16M','64M','256M']
-pyplot.xticks(x,labels)
+#pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,1],marker=".");
+#pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,2],marker=".");
+#labels = [1,4,16,256,'1K','4K','16K','64K','256K','1M','4M','16M','64M','256M']
+#pyplot.xticks(np.log(data[1:,0]/np.log(4.)),labels)
 #icolumnFirst = pyplot.scatter(ifort[1:,0], ifoty[1:,2],marker=".");
 pyplot.legend((mytime,other),('mytime','notmytime'));
 pyplot.savefig('compare.png')
