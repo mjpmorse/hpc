64aeed2402d93fb38e2a806bca043703ade5a83d
Michael Morse <mjmorse3@srv-k07-14.cbls.ccr.buffalo.edu>
2017-09-28 17:13:49 -0400

    python fix

--- hw1/problem4/ploter.py
+++ hw1/problem4/ploter.py
@@ -7,8 +7,8 @@ data = np.genfromtxt("problem4.txt",delimiter=",");
 #print mat1[1:3,:]
 mytime = pyplot.scatter(np.log(data[1:,0]/np.log(4.)), data[1:,1],marker=".");
 other  = pyplot.scatter(np.log(data[1:,0]/np.log(4.)), data[1:,2],marker=".");
-pyplot.(np.log(data[1:,0]/np.log(4.)), data[1:,1],marker=".");
-pyplot.(np.log(data[1:,0]/np.log(4.)), data[1:,2],marker=".");
+pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,1],marker=".");
+pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,2],marker=".");
 labels = [1,4,16,256,'1K','4K','16K','64K','256K','1M','4M','16M','64M','256M']
 pyplot.xticks(x,labels)
 #icolumnFirst = pyplot.scatter(ifort[1:,0], ifoty[1:,2],marker=".");
