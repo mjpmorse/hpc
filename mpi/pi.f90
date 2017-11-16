4611fa79096a7e7d681a7def8329322087c1801a
Michael Morse <mjmorse3@srv-k07-14.cbls.ccr.buffalo.edu>
2017-11-16 10:27:47 -0500

    something

--- mpi/pi.f90
+++ mpi/pi.f90
@@ -3,7 +3,7 @@
          implicit none
          double precision PI25DT
          parameter (PI25DT = 3.141592653589793238462643d0)
-         double precision mypi,pi,h,sum,x,f,a,sum2.pi2
+         double precision mypi,pi,h,sum1,x,f,g,a,sum2,pi2,mypi2
          integer n,myid,numprocs,i,ierr
 
 ! function to integrate
@@ -14,27 +14,28 @@
          call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)
          call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
 
-!10       if (myid .eq. 0) then
+10       if (myid .eq. 0) then
 !           write(*,*) 'Enter number of intervals: (0 quits) '
 !           read(*,*)   n
-!         end if
-         n = 1d10
+            n = 1d5
+         end if
+!         n = 1d5
 
          call MPI_BCAST(n,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
 
          if (n .le. 0) goto 30
 
          h = 1.0d0/n
-         sum = 0.0d0
+         sum1 = 0.0d0
          sum2 = 0.0d0
          do 20 i = myid +1,n,numprocs
            x = h * (dble(i) - 0.5d0)
-           sum = sum +f(x)
+           sum1 = sum1 +f(x)
            sum2 = sum2 + g(x)
            ! rectangles are number of proc apart
 
 20       continue
-         mypi = h*sum
+         mypi = h*sum1
          mypi2 = h*sum2
 
 ! collect all the partial sums
@@ -45,7 +46,7 @@
 
           if (myid .eq. 0) then
             write(*,*) 'pi from Gropp is ', pi,' Error is ',abs(pi-PI25DT)
-            write(*,*) 'pi from HW is ', pi2,' Error is ',abs(pi-PI25DT)
+            write(*,*) 'pi from HW is ', pi2,' Error is ',abs(pi2-PI25DT)
           end if
 !         goto 10
 30        call MPI_FINALIZE(ierr)
