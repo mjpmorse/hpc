4a9ad645ac3c6176da8ed1a74378c86f80d26681
Michael Morse <mjmorse3@buffalo.edu>
2017-11-28 09:36:36 -0500

    changes

--- hw2/errormod.f90
+++ hw2/errormod.f90
@@ -1,19 +1,17 @@
         module errorMod
         use mpi
-        implicit none
 
         contains
 
         subroutine error_check(error)
-!        Adapted from Nick Maclaren at csx.cam.ac.uk 
+!        Adapted from Nick Maclaren at csx.cam.ac.uk
          INTEGER :: error, length,temp
          CHARACTER(LEN = MPI_MAX_ERROR_STRING) :: message
-         
+
          if (error .ne. MPI_SUCCESS) then
            call MPI_ERROR_STRING(error,message,length,temp)
            print *,message(1,length)
            CALL MPI_ABORT(MPI_COMM_WORLD,1,temp)
          end if
-
        end subroutine
        end module
--- hw2/question1/problem1.f90
+++ hw2/question1/problem1.f90
@@ -1,20 +1,18 @@
-program helloworld
-   use mpi
-   use errorMod
-   implicit none
-   integer ierr,rank,numprocs
+      program helloworld
+       use mpi
+       use errorMod
+       implicit none
+       integer error,rank,numprocs
+       call MPI_INIT(error)
+       call MPI_COMM_SET_ERRHANDLER(MPI_COMM_WORLD,MPI_ERRORS_RETURN,error)
+       call MPI_COMM_RANK(MPI_COMM_WORLD,rank,error)
+       call ERROR_CHECK(error)
+       call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,error)
+       call ERROR_CHECK(ierr)
 
-   call MPI_INIT(ierr)
-   call MPI_COMM_SET_ERRHANDLER(MPI_COMM_WORLD,MPI_ERRORS_RETURN,ierr) 
-   call ERROR_CHECK(ierr)
-   call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
-   call ERROR_CHECK(ierr)
-   call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
-   call ERROR_CHECK(ierr)
+       write(*,*) 'Hello world myid, and numprocs',rank,numprocs
 
-   write(*,*) 'Hello world myid, and numprocs',rank,numprocs
+       call MPI_FINALIZE(error)
+       call ERROR_CHECK(error)
 
-   call MPI_FINALIZE(ierr)
-   call ERROR_CHECK(ierr)
-
- end program
+      end program
