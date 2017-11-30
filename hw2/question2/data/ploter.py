35bec5c064600a3c5f84a516fe534bb6f93761af
Michael Morse <mjmorse3@srv-k07-14.cbls.ccr.buffalo.edu>
2017-11-30 14:48:30 -0500

    changes

--- hw2/question2/data/ploter.py
+++ hw2/question2/data/ploter.py
@@ -1,15 +1,9 @@
 from matplotlib import pyplot;
 #from pylab import genfromtxt;
 import numpy as np
-#data = np.genfromtxt("problem4.txt",delimiter=",");
-data = np.genfromtxt("data2.dat",delimiter=",");
-mytime = pyplot.scatter(np.log(4.*data[1:,0]/np.log(4.)), data[1:,1]*10.**3,marker=".");
-#pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,1]);
-#pyplot.plot(np.log(data[1:,0]/np.log(4.)), data[1:,2]);
-#labels = [1,4,16,256,'1K','4K','16K','64K','256K','1M','4M','16M','64M','256M']
-#pyplot.xticks(np.log(data[1:,0]/np.log(4.)),labels)
+data = np.genfromtxt("dataq2.dat",delimiter=",");
+mytime = pyplot.scatter(np.log(data[1:,0])/np.log(4.)), data[1:,1]*10.**3,marker=".");
 pyplot.xlabel('Log_4 Vector Size in Bytes')
 pyplot.ylabel('Time(ms)')
-#pyplot.legend((mytime),('Simple BLAS','NETLIB BLAS','Simple BLAS MKL','NETLIB BLAS MKL'));
 pyplot.title("Ping-Pong")
 pyplot.savefig('pingpong.png')
--- hw2/question2/problem2.f90
+++ hw2/question2/problem2.f90
@@ -3,7 +3,7 @@
          use mpi
          implicit none
          integer :: length
-         integer, dimension(length) :: vector
+         integer(kind = 8), dimension(length) :: vector
          integer :: error,rank
          integer ::status(MPI_STATUS_SIZE)
            CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank,error)
@@ -44,7 +44,7 @@
          integer :: maxlength,length,i
          integer :: status(MPI_STATUS_SIZE)
          integer ,dimension(:),allocatable :: vector
-         real(kind = qp) :: starttime, endtime, time
+         real(kind = dp) :: starttime, endtime, time
          real(kind = qp) :: quadone,quadsmallloop
          integer :: smallloop
          character(len = 32) :: datafile
@@ -58,9 +58,8 @@
          call ERROR_CHECK(error)
          call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,error)
          call ERROR_CHECK(error)
-
-! default interger in F90 is 4 bytes so we need maxlength = 2E6 to get a max of 8MBytes          
-         maxlength = 2001000
+          
+         maxlength = 100000000
          quadone = 1
 !set up data file
          if(rank .eq. 0) then
@@ -69,7 +68,7 @@
            write(1,*) 'vector size (bytes) ,','time (s)'
          end if
 
-         do length = 1, maxlength, 100
+         do length = 1, maxlength+1, 100
            allocate(vector(length))
            vector(:)= 0
            if(rank.eq.0) then 
@@ -79,10 +78,13 @@
            if(length .lt. 10000) then 
               do smallloop = 1,1000 
                   call pingpong(vector,length)
+                  if(rank.eq.1) then
+                    vector(:) = 0
+                  end if
               end do
-              if(rank.eq.0) then
+              if(rank .eq.0) then
                  endtime =  MPI_WTIME()
-                 time = (endtime - starttime)/1000
+                 time = (endtime - starttime)/real(1000,dp)
                  write(1,*) length,',',time
                end if
            else 
