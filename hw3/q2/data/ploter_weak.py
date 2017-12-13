3d8923c8399b56caace11a150033b5169c3202c8
Michael Morse <mjmorse3@buffalo.edu>
2017-12-13 12:35:35 -0500

    report

--- hw3/q2/data/ploter_weak.py
+++ hw3/q2/data/ploter_weak.py
@@ -3,39 +3,44 @@ from matplotlib import colors
 from scipy.stats import linregress
 #from pylab import genfromtxt;
 import numpy as np
-data = np.genfromtxt("q2_strong.dat",delimiter=",");
+data = np.genfromtxt("q2_weak.dat",delimiter=",");
 size = (data[:,0]-1)
 time = data[:,1]
 plt.figure(0)
 plt.scatter(size,time,marker="o",color='blue' );
 plt.plot(size,time,color='black')
-plt.xlabel('number of nodes')
+plt.xlabel('number of cores')
 plt.ylabel('time (s)')
-plt.title('time vs nodes')
+plt.title('time vs cores')
 ##plt.show()
-plt.savefig('timevsnodes.png')
+plt.savefig('timeq2_weak.png')
 
 plt.figure(1)
 speedup = data[0,1]/time
 plt.scatter(size,speedup,marker="o",color='blue' );
+print linregress(1./size,1./speedup)
 plt.plot(size,speedup,color='black')
-plt.xlabel('number of nodes')
+plt.xlabel('number of cores')
 plt.ylabel('speedup')
-plt.title('speedup vs nodes')
-plt.savefig('speedupvsnode.png')
+plt.title('speedup vs cores')
+plt.savefig('speedupq2_weak.png')
 
 plt.figure(2)
 speedup = data[0,1]/time
 plt.scatter(1./size,1./speedup,marker="o",color='blue' );
-m,b = np.polyfit(1./size,1./speedup,1)
-print m,b
-print linregress(1./size[2:],1./speedup[2:])
 plt.plot(1./size,1./speedup,color='black')
 plt.xlabel('1/(number of cores)')
 plt.ylabel('1/speedup')
 plt.title('1/speedup vs 1/cores')
-plt.savefig('inversespeedup.png')
+plt.savefig('inversespeedupq2_weak.png')
 
+plt.figure(3)
+plt.scatter(size,speedup*size,marker="o",color='blue' );
+plt.plot(size,speedup*size,color='black')
+plt.xlabel('number of cores')
+plt.ylabel('speedup*number of cores')
+plt.title('speedup*cores vs cores')
+plt.savefig('speed_coresq2_weak.png')
 print 'Karp-Flatt Metric'
 f = speedup
 for i in range(1,len(speedup)):
